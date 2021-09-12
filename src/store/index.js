5aed11474219d1e1e105c35baffd3d6f3dac6b09
AlexiSkyline <ilegal_sprite@outlook.com>
2021-09-11 22:52:46 -0500

    Getting the characters

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,26 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    character: [],
+    characterFilter: []
   },
   mutations: {
+    setCharacters( state, payload ) {
+      state.character = payload;
+    },
+    setCharactersFilter( state, payload ) {
+      state.characterFilter = payload;
+    }
   },
   actions: {
+    async getCharacters({ commit }) {
+      try {
+        const response = await fetch( 'https://rickandmortyapi.com/api/character' );
+        const data = await response.json();
+      } catch (error) {
+        console.log( error );
+      }
+    }
   },
   modules: {
   }
